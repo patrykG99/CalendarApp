ebc9c566edf0b88eaaf27766549b31229f734204
qxeer <qxeer84@gmail.com>
2023-10-04 01:14:41 +0200

    Added weather to calendar days

--- frontend/weatherfrontend/src/components/calendar.component.js
+++ frontend/weatherfrontend/src/components/calendar.component.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import "./styles.scss"
 
 const months = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'];
@@ -9,11 +9,16 @@ const getDayName = (day, month, year) => {
     return days[date.getDay()];
 };
 
+
+
 const Calendar = () => {
     const [date, setDate] = useState(new Date());
+    const [weather,setWeather] = useState('')
+    const [hasLoaded, setHasLoaded] = useState(false)
 
     const currentMonth = date.getMonth();
     const currentYear = date.getFullYear();
+    const currentDay = date.getDate();
     const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
 
     const daysOfMonth = Array.from({ length: daysInMonth }, (_, i) => i + 1);
@@ -32,24 +37,59 @@ const Calendar = () => {
         setDate(new Date(currentYear, currentMonth - 1, 1));
     };
 
+    useEffect(() => {
+        async function fetchData() {
+            const url = `http://localhost:8080/api/weatherData/Bydgoszcz`
+            const options = {
+                method: 'GET'
+            }
+            let response = await fetch(url,options)
+            let actualWeather =await response.json()
+            setWeather(actualWeather)
+            setHasLoaded(true)
+        }
+        fetchData();
+
+
+
+    }, []);
+
+    const getWeatherForDay = (day, month, year) =>{
+        const allDays = weather.forecast.forecastday;
+
+        const found = allDays.find(w => {
+            const [wYear,wMonth,wDay] = w.date.split('-').map(Number)
+            return wDay === day && wMonth === month && wYear === year
+
+
+        })
+        console.log(found)
+        return found ? found.day.maxtemp_c : null
+
+    }
+
+
     return (
         <>
             <div><h2>{months[currentMonth]}</h2></div>
-        <div className={"calendar"}>
-            {daysOfMonth.map((day, index) => (
-                <div
-                    key={index}
-                    className="dayCard"
-                    data-day={day}
-                    data-month={currentMonth}
-                    onClick={handleDayClick}
-                >
-                    {day}, {getDayName(day, currentMonth, currentYear)}
+            {hasLoaded ? <>
+                <div className={"calendar"}>
+                    {daysOfMonth.map((day, index) => (
+                        <div
+                            key={index}
+                            className="dayCard"
+                            data-day={day}
+                            data-month={currentMonth}
+                            onClick={handleDayClick}
+                        >
+                            {day}, {getDayName(day, currentMonth, currentYear)},{<div>{getWeatherForDay(day, currentMonth + 1, currentYear)}</div>}
+                        </div>
+                    ))}
                 </div>
-            ))}
-        </div>
-            <div><button onClick={handlePrevMonth}>Poprzedni</button>
-                <button onClick={handleNextMonth}>Następny</button></div>
+                <div><button onClick={handlePrevMonth}>Poprzedni</button>
+                    <button onClick={handleNextMonth}>Następny</button></div>
+            </>:null}
+
         </>
     );
 };
